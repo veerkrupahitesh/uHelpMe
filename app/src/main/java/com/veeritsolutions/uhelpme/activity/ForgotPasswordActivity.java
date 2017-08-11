d3b2f331d1abb59975725ab3e6e2a2de1cec089a
Hitesh <veerkrupa.hitesh@gmail.com>
2017-08-11 19:04:59 +0530

    4nd commit

--- app/src/main/java/com/veeritsolutions/uhelpme/activity/ForgotPasswordActivity.java
+++ app/src/main/java/com/veeritsolutions/uhelpme/activity/ForgotPasswordActivity.java
@@ -94,6 +94,10 @@ public class ForgotPasswordActivity extends AppCompatActivity implements OnClick
             case R.id.img_back_header:
               /*  Intent intent = new Intent(this, SignInActivity.class);
                 startActivity(intent);*/
+
+
+
+
                 finish();
                 break;
 
